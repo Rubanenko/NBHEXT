a013bf01e151785c7aa2d19e86a00ac508ea0895
Maxim Molchanov <max@looksery.com>
2015-10-23 03:36:13 +0300

    Added delta calculation. Added api queries.

--- NBHEXT/showPreRatings.js
+++ NBHEXT/showPreRatings.js
@@ -1,8 +1,13 @@
 var partyNum = 0;
+var ratings = [];
+var places = [];
+var deltas = [];
 
 function partyRatingHtml()
 {
-    var delta = 1;
+    var delta = 0;
+    if (partyNum > 0)
+        delta = Math.round(deltas[partyNum - 1]);
     var text;
     if (partyNum == 0)
     {
@@ -24,10 +29,29 @@ function partyRatingHtml()
 
 function showPreRatings()
 {
-    $(".standings").find("tr").first().find("th").last().removeClass("right");
-    $(".standings").find("tr").find("td").removeClass("right");
-    $(".standings").find("tr").append(partyRatingHtml);
-    $(".standings").find("tr").last().find("td").last().html("<td class='smaller bottom dark right'> </td>");
+    $.getJSON("http://codeforces.com/api/contest.standings?contestId=587",
+        function(data)
+        {
+            var handles = "";
+            for (var i = 0; i < data.result.rows.length; ++i)
+            {
+                places[i] = data.result.rows[i].rank;
+                handles += data.result.rows[i].party.members[0].handle + ";";
+            }
+            $.getJSON("http://codeforces.com/api/user.info?handles=" + handles,
+                function(data)
+                {
+                    for (var i = 0; i < data.result.length; ++i)
+                        ratings[i] = data.result[i].rating;
+                    deltas = CalculateRatingChanges(ratings, places, []);
+                    $(".standings").find("tr").first().find("th").last().removeClass("right");
+                    $(".standings").find("tr").find("td").removeClass("right");
+                    $(".standings").find("tr").append(partyRatingHtml);
+                    $(".standings").find("tr").last().find("td").last().html("<td class='smaller bottom dark right'> </td>");
+                }
+            );
+        }
+    );
 }
 
 showPreRatings();
